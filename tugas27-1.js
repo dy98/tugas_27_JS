b8b5ddb76cc01c872ad173c64c93f319a6743d24
Dwi Yulianto <98dwiyulianto@gmail.com>
2020-10-17 16:33:54 +0700

    tugas 27 JS
    tugas untuk Niomic

---
+++ tugas27-1.js
@@ -0,0 +1,9 @@
+function gabungKata() {
+    let no = "No";
+    let mine = "rumah saya";
+    let number = 30;
+    let alfabetA = "A";
+
+    console.log(String(no + ' ' + mine + ' ' + number + ' ' + alfabetA));
+}
+gabungKata();
---
+++ tugas27-2.js
@@ -0,0 +1,7 @@
+function gabungKata2() {
+    let address = "Jalan pangeran Tirtayasa No";
+    let numbers = 70;
+
+    console.log(String(address + ' ' + numbers));
+}
+gabungKata2();
